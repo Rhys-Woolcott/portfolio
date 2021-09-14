19463d0f7ad04c166435b75250a3cb37679a5078
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-09-14 21:46:22 +0100

    Fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line no-undef
 module.exports = {
- env: {
-  browser: true,
-  es2021: true,
- },
- extends: [
-  'eslint:recommended',
-  'plugin:react/recommended',
-  'plugin:@typescript-eslint/recommended',
- ],
- parser: '@typescript-eslint/parser',
- parserOptions: {
-  ecmaFeatures: {
-   jsx: true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  ecmaVersion: 12,
-  sourceType: 'module',
- },
- plugins: ['react', '@typescript-eslint'],
- rules: {
-  indent: ['error', 1],
-  'linebreak-style': ['error', 'windows'],
-  quotes: ['error', 'single'],
-  semi: ['error', 'always'],
-  'no-use-before-define': 'off',
-  '@typescript-eslint/no-use-before-define': ['error'],
-  'max-len': ['warn', { code: 80 }],
-  'react/react-in-jsx-scope': 'off',
- },
- settings: {
-  'react': {
-   'version': 'detect'
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'max-len': ['warn', { code: 80 }],
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    'react': {
+      'version': 'detect'
+    }
   }
- }
 };
